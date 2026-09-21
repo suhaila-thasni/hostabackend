36c3ed5a3cee6a35479c84f97f33c1ee77fbee51
suhaila thasni <suhailathasni0@gmail.com>
2026-09-21 13:29:58 +0530

    updated

--- services/hospital-service/migrations/20260921000001-change-device-type-to-enum.js
+++ services/hospital-service/migrations/20260921000001-change-device-type-to-enum.js
@@ -1,18 +1,24 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     // Change deviceType from STRING to ENUM('face', 'rfid', 'fingerprint')
-    await queryInterface.changeColumn('rfid_devices', 'deviceType', {
-      type: Sequelize.ENUM('face', 'rfid', 'fingerprint'),
+    await queryInterface.sequelize.query(`
+      UPDATE "rfid_devices"
+      SET "deviceType" = LOWER("deviceType")
+      WHERE LOWER("deviceType") IN ('face', 'rfid', 'fingerprint');
+    `);
+
+    await queryInterface.changeColumn("rfid_devices", "deviceType", {
+      type: Sequelize.ENUM("face", "rfid", "fingerprint"),
       allowNull: false,
     });
   },
 
   async down(queryInterface, Sequelize) {
     // Revert back to STRING
-    await queryInterface.changeColumn('rfid_devices', 'deviceType', {
+    await queryInterface.changeColumn("rfid_devices", "deviceType", {
       type: Sequelize.STRING,
       allowNull: false,
     });
